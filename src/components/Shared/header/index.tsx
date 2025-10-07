26fdd8fa6e9baacdbdd7b6c42e017efd23227a42
tsiks24 <tsiks24@gmail.com>
2025-10-07 08:57:58 +0200

    #

---
+++ src/components/Shared/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from './Head'
+
+function Footer() {
+  return (
+    <footer>
+    <Head/>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Eastwind-Trust'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Invest Smart, Live Better'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A real estate website built with next js' 

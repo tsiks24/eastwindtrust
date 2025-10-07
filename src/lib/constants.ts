347be6a2eeb7899dee453029920010f5f75abffc
tsiks24 <tsiks24@gmail.com>
2025-10-07 10:15:11 +0200

    #

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,31 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Eastwind-Trust'
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Invest Smart, Live Better'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A real estate website built with next js' 
+
+
+export const navLinks = [
+           {
+         name:"Home",
+         href: '/',
+       },
+       {
+         name:"About",
+         href: '/about',
+       },
+       {
+         name:"Projects",
+         href: '/projects',
+       },
+       {
+         name:"Our-Team",
+         href: '/team',
+       },
+       {
+         name:"Contact",
+         href: '/contact',
+       }
+      
+     ]
+  
+
+
---
+++ src/lib/data.ts
@@ -0,0 +1,29 @@
+
+
+
+const data: Data = {
+    headerMenus:[
+        {
+           name:"Home",
+           href:'/'
+        },
+         {
+           name:"About Us",
+           href:'/about'
+        },
+         {
+           name:"Projects",
+           href:'/projects'
+        },
+         {
+           name:"Our Team",
+           href:'/team'
+        },
+         {
+           name:"Contact Us",
+           href:'/contact'
+        },
+    ],
+}
+
+export default data
